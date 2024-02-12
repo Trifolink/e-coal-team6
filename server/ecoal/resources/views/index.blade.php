77f57a27fc09e560947e6d29c99434a01a8dd0e6
Novais <abigail62810@gmail.com>
2024-02-12 23:16:01 +0100

    index + routes

---
+++ server/ecoal/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-coal</title>
+</head>
+<body>
+    <h1>Je suis ta mere</h1>
+
+    <img src="https://images.bfmtv.com/5E67zRGTtNMCNSbGC5F84_QgfMI=/3x0:947x531/944x0/images/Wakfu-va-avoir-droit-a-une-saison-4-grace-a-ses-fans-371452.jpg" alt="oui">
+</body>
+</html>
