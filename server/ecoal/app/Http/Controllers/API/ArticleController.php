a1aff77b071c9b20e51bbcc4e2bce0e899bca334
Novais <abigail62810@gmail.com>
2024-02-13 10:30:16 +0100

    Ajout Article

--- server/ecoal/app/Http/Controllers/API/ArticleController.php
+++ server/ecoal/app/Http/Controllers/API/ArticleController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Models\Tag;
+
 
 class ArticleController extends Controller
 {
@@ -22,7 +24,33 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validation des données de la requête (à adapter selon vos besoins)
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+            'thumbnailURL' => 'required|url',
+            'mediaType' => 'nullable',
+            'mediaURL' => 'nullable|url',
+            'leadStory' => 'required|boolean',
+            'tags' => 'required|array',
+            'tags.*' => 'exists:tags,id', // Vérifier que chaque tag existe en base de données
+        ]);
+    
+        // Création de l'article
+        $article = Article::create([
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+            'thumbnailURL' => $validatedData['thumbnailURL'],
+            'mediaType' => $validatedData['mediaType'],
+            'mediaURL' => $validatedData['mediaURL'],
+            'leadStory' => $validatedData['leadStory'],
+        ]);
+    
+        // Attachement des tags à l'article
+        $article->tags()->attach($validatedData['tags']);
+    
+        // Retour de la réponse JSON
+        return response()->json($article, 201);
     }
 
     /**
--- server/ecoal/resources/views/index.blade.php
+++ server/ecoal/resources/views/index.blade.php
@@ -11,8 +11,12 @@
     <img src="https://resize.elle.fr/original/var/plain_site/storage/images/elle-a-table/les-dossiers-de-la-redaction/dossier-de-la-redac/plat-familial-sans-four/97674745-2-fre-FR/15-recettes-de-plats-familiaux-sans-four.jpg" alt="oui">
 
     <div id="articles-container">
-
     </div>
+
+    <div>
+        <button>OK</button>
+    </div>
+
 
     <script>
         fetch('/api/articles')
