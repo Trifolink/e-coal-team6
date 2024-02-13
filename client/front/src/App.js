619668549028a2cdde5fd5270f4a3b6670ecbad1
Nathan COQUART <coquartnathan@gmail.com>
2024-02-13 16:00:26 +0100

    link

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -2,13 +2,33 @@ import styles from './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Home from './Home/home';
+import Articles from './Articles/Articles';
+import {Route, Link, Routes} from "react-router-dom";
+
 
 function App() {
   return (
     <div className={styles.App}>
       <Header/>
-      <br></br>
-      <Home></Home>
+      {/* <br></br> */}
+
+
+
+      <nav>
+    <Link to="/">Home</Link>
+    <Link to="/articles">Articles</Link>
+  </nav>
+
+  <Routes>
+    <Route exact={true} path="/" element={<Home/>}/>
+    <Route exact={true} path="/articles" element={<Articles />} />
+  </Routes>
+
+
+
+
+
+      {/* <Home></Home> */}
       <Footer/>
     </div>
   );
