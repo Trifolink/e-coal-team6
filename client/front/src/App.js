a9b592112fc97577ad5f8e220654841066dbc422
Maison Remy <remy_maison@ens.univ-artois.fr>
2024-02-15 09:17:36 +0100

    login/register/ajtarticle

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,12 +1,15 @@
 import styles from './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Login from './Login/Login';
+import Register from './Register/Register.jsx';
+import AjoutArticle from './AjoutArticle/AjoutArticle.jsx';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header/>
-      <br></br>
+      <AjoutArticle/>
       <Footer/>
     </div>
   );
