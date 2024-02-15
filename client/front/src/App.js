199c3e00be2da8f6398198be682625eb0b90fb75
Nathan COQUART <coquartnathan@gmail.com>
2024-02-15 13:23:41 +0100

    home

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -4,10 +4,7 @@ import Footer from './Footer/Footer';
 
 import Login from './Login/Login';
 import Register from './Register/Register.jsx';
-import AjoutArticle from './AjoutArticle/AjoutArticle.jsx';
-import Articles from './Articles/Articles.jsx';
 import Listes from './Listes/Listes';
-import Homepage from './Homepage/Homepage';
 
 import Home from './Home/home';
 import Articles from './Articles/Articles';
@@ -21,7 +18,6 @@ function App() {
     <div className={styles.app}>
       <Header/>
 
-      <Homepage/>
 
 
       <Routes>
