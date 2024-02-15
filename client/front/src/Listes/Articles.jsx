a43dc12675826e7aaffa1bcfd888c481cef8c06d
Nathan COQUART <coquartnathan@gmail.com>
2024-02-15 23:10:44 +0100

    d

--- client/front/src/Listes/Articles.jsx
+++ client/front/src/Listes/Articles.jsx
@@ -33,7 +33,7 @@ function ArticlesList() {
     return (
         <div className={styles.articles} style={{ backgroundImage:`url(${background})` }}>
             <div className={styles.box}>
-            <h1 className={styles.title}>Liste des articles</h1>
+            <h1 className={styles.title}>List of articles</h1>
             <input className={styles.seachbar}
                 type="text"
                 placeholder="Search"
